4019dad2a4912771e698c4a0e01050ba174bcdd9
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-04-17 20:00:19 -0500

    Add files via upload

---
+++ EstructurasConArreglos/EstructurasConArreglos/Inventario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasConArreglos
+{
+    class Inventario
+    {
+        private Producto[] _pro;
+      
+
+
+
+
+        public void agregarProducto(Producto pro)
+        {
+
+            for (int i = _pro.Length - 1; i >= 0 + 1; i--)
+            {
+                _pro[i] = _pro[i - 1];
+            }
+            _pro[0] = pro;
+        }
+
+        public Producto buscar(int cod)
+        {
+            for (int i = 0; i < _pro.Length-1; i++)
+            {
+                if (_pro[i].codigo == cod)
+                {
+                    return _pro[i];
+                }
+               
+            }
+
+            return null;
+        }
+
+        public void eliminarProducto(int cod)
+        {
+            for(int i = 0; i < _pro.Length -1; i++)
+            {
+                if (_pro[i].codigo == cod)
+                {
+                    _pro[i] = null;
+                }
+                   
+            }
+        }
+
+     
+
+
+
+     
+
+    }
+}
---
+++ EstructurasConArreglos/EstructurasConArreglos/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasConArreglos
+{
+    class Producto
+    {
+        private int _codigo;
+        private int _cantidad;
+        private int _costo;
+
+        private string _nombre;
+        private string _desc;
+
+        public Producto(int cd,int cnt,int cst, string nm, string desc)
+        {
+            _codigo = cd;
+            _cantidad = cnt;
+            _costo = cst;
+            _nombre = nm;
+            _desc = desc;
+        }
+
+        public int getCodigo { get { return _codigo; } }
+        public int getCantidad { get { return _cantidad; } }
+        public int getCosto { get { return _costo; } }
+        public string getNombre { get { return _nombre; } }
+        public string getDesc { get { return _desc; } }
+
+    }
+}
